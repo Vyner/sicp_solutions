379a7a24969c353f0fafb7e2b84228b9b43aa2d5
vyner <vynercylma@outlook.com>
2019-12-05 18:52:50 +0800

    temp commit queens

--- app/queens.rkt
+++ app/queens.rkt
@@ -9,18 +9,24 @@
         (filter
          (lambda (positions) (safe? k positions))
          (my-flatmap
+         ;在前k-1列放置k-1个皇后的一种方式
           (lambda (rest-of-queens)
-            (map (lambda (new-rows)
+            (map (lambda (new-row)
+                   ; 将新的行列格局加入格局集合
                    (adjoin-position new-row k rest-of-queens))
                  (enumerate-interval 1 board-size)))
           (queen-cols (- k 1))))))
   (queen-cols board-size))
+
+(define test-rest-of-queens (list (list (list 1 1) (list 2 2) (list 3 3))))
+
+(define (adjoin-position n k rest-of-queens)
+  (append rest-of-queens (list(list n k))))
+
+(define empty-board (list(list(list))))
+
+(define (safe? k positions)
+  (is-already-exist-y
   
 
-(lambda (rest-of-queens)
-            (map (lambda (new-rows)
-                   (adjoin-position new-row k rest-of-queens))
-                 (enumerate-interval 1 board-size)))
-
-(lambda (new-rows)
-                   (adjoin-position new-row k rest-of-queens))
+;(((1,1), (2,2), (3,3)) , ((1, 2), (2, 3), (3, 1)), ((1, 3), (2, 1), (3, 2)))
